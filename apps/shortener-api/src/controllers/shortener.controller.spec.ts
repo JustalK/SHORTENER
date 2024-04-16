eb9d18144db4c1481ad08aac15c5b1df8fb6f3ad
Justal Kevin <justal.kevin@gmail.com>
2024-04-16 17:32:05 +0200

    :sparkle: Improving unit test shortener controller

--- apps/shortener-api/src/controllers/shortener.controller.spec.ts
+++ apps/shortener-api/src/controllers/shortener.controller.spec.ts
@@ -2,37 +2,80 @@ import express from 'express';
 import { ShortenerType } from '@root/types/src';
 import { ExceptionType } from '@root/types/src';
 import { ShortenerController } from './shortener.controller';
+import { ShortenerServiceType } from '@interfaces/shortener.interface';
+import { ExceptionServiceType } from '@interfaces/error.interface';
+import { ShortenerDaoType } from '@interfaces/shortener.interface';
 import shortenerModel from '@models/shortener.model';
 const httpMocks = require('node-mocks-http');
 
 describe('App', () => {
+  let shortenerService: ShortenerServiceType;
+  let exceptionService: ExceptionServiceType;
+  let shortenerDao: ShortenerDaoType;
+  beforeAll(() => {
+    shortenerService = {
+      shortenUrl: async (longURL: string) => {
+        return new shortenerModel({ longURL }) as ShortenerType;
+      },
+      isExpired: () => {
+        return true;
+      },
+      getShortenUrl: async () => {
+        return new shortenerModel() as ShortenerType;
+      },
+    };
+    exceptionService = {
+      createException: () => {
+        return {
+          code: 'X0000',
+          message: 'Test Message',
+        } as ExceptionType;
+      },
+    };
+    shortenerDao = {
+      saveShortURLValidator: async () => {
+        return {
+          code: 'X0000',
+          message: 'Test Message',
+        } as ExceptionType;
+      },
+      redirectToLongURLValidator: () => {
+        return {
+          code: 'X0000',
+          message: 'Test Message',
+        } as ExceptionType;
+      },
+    };
+  });
   it('should render successfully', async () => {
     const shortenerController = ShortenerController.getInstance({
-      shortenerService: {
-        shortenUrl: async (longURL: string) => {
-          return new shortenerModel({ longURL }) as ShortenerType;
-        },
-        isExpired: () => {
-          return true;
-        },
-        getShortenUrl: async () => {
-          return new shortenerModel() as ShortenerType;
-        },
+      shortenerService,
+      exceptionService,
+      shortenerDao,
+    });
+
+    const request = httpMocks.createRequest({
+      method: 'POST',
+      url: '/user/42',
+      body: {
+        longURL: 'azazeae',
       },
-      exceptionService: {
-        createException: () => {
-          return {
-            code: 'X0000',
-            message: 'Test Message',
-          } as ExceptionType;
-        },
-      },
+    });
+    const response = httpMocks.createResponse({
+      eventEmitter: require('events').EventEmitter,
+    });
+    await shortenerController.saveShortURL(request, response);
+    expect(response.statusCode).toEqual(500);
+    expect(response._getData().error.code).toEqual('X0000');
+    expect(response._getData().error.message).toEqual('Test Message');
+  });
+  it('should render successfully 2', async () => {
+    const shortenerController = ShortenerController.getInstance({
+      shortenerService,
+      exceptionService,
       shortenerDao: {
         saveShortURLValidator: async () => {
-          return {
-            code: 'X0000',
-            message: 'Test Message',
-          } as ExceptionType;
+          return null;
         },
         redirectToLongURLValidator: () => {
           return {
@@ -53,12 +96,9 @@ describe('App', () => {
     const response = httpMocks.createResponse({
       eventEmitter: require('events').EventEmitter,
     });
-    const result = await shortenerController.saveShortURL(request, response);
-    expect(result.statusCode).toEqual(500);
-    expect(response._getData().error.code).toEqual('X0000');
-    expect(response._getData().error.message).toEqual('Test Message');
-  });
-  it('should render successfully', () => {
-    expect(true).toBeTruthy();
+    await shortenerController.saveShortURL(request, response);
+    expect(response.statusCode).toEqual(200);
+    expect(response._getData().longURL).toEqual('azazeae');
+    expect(response._getData().shortURL).toBeDefined();
   });
 });
